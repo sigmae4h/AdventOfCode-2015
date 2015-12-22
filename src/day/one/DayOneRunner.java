73938b31062ac0132d0cb382d8381cbf51a49803
Chris Decker <sigma.e4h@gmail.com>
2015-12-22 09:36:04 -0500

    Added DayOne objective in comments, DayOneRunner, and DayOneInuput.txt

---
+++ src/day/one/DayOneRunner.java
@@ -0,0 +1,19 @@
+package day.one;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class DayOneRunner {
+
+	public static void main(String[] args) {
+		String input = "";		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("DayOneInput.txt"))) {
+			input = br.readLine();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		System.out.println("Ending floor: " + DayOne.getFloor(input));
+	}
+}
