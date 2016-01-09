3bba20acdba68867cb0079b795b6b9317adc6961
Chris Decker <sigma.e4h@gmail.com>
2016-01-09 15:42:27 -0500

    Day Seven, part 2

--- src/day/seven/DaySevenRunner.java
+++ src/day/seven/DaySevenRunner.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class DaySevenRunner {
 	public static void main(String[] args) {
+		int signalA = 0;
+		
 		try (Scanner input = new Scanner(new File("Resources/Input/Seven.txt"), "UTF-8")) {
 			DaySeven daySeven = new DaySeven();
 
@@ -13,8 +15,25 @@ public class DaySevenRunner {
 			}
 
 			daySeven.runCircuit();
+			
+			signalA = daySeven.getWire("a");
 
-			System.out.println("Wire a's final signal: " + daySeven.getWire("a"));
+			System.out.println("Wire a's final signal: " + signalA);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		try (Scanner input = new Scanner(new File("Resources/Input/Seven.txt"), "UTF-8")) {
+			DaySeven daySeven = new DaySeven();
+
+			while (input.hasNext()) {
+				daySeven.setWire(input.nextLine());
+			}
+
+			daySeven.setWire(signalA + " -> b");
+			daySeven.runCircuit();
+
+			System.out.println("Wire a's final signal, part 2: " + daySeven.getWire("a"));
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
