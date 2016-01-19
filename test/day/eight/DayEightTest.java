9a7677513bbce4c92b285e8c0bbbe5c1ea68be27
Chris Decker <sigma.e4h@gmail.com>
2016-01-18 19:26:58 -0500

    Cleaned up Day Eight tests

--- test/day/eight/DayEightTest.java
+++ test/day/eight/DayEightTest.java
@@ -13,44 +13,48 @@ public class DayEightTest {
 
 	@Test
 	public void test1() {
-		Integer expectedLength = 2, expectedData = 0;
+		Integer expectedLength = 2, expectedData = 0, expectedEncoded = 6;
 		Integer results[] = DayEight.process("\"\"");
 
 		assertEquals(expectedLength, results[0]);
 		assertEquals(expectedData, results[1]);
+		assertEquals(expectedEncoded, results[2]);
 	}
 
 	@Test
 	public void test2() {
-		Integer expectedLength = 5, expectedData = 3;
+		Integer expectedLength = 5, expectedData = 3, expectedEncoded = 9;
 		Integer results[] = DayEight.process("\"abc\"");
 
 		assertEquals(expectedLength, results[0]);
 		assertEquals(expectedData, results[1]);
+		assertEquals(expectedEncoded, results[2]);
 	}
 
 	@Test
 	public void test3() {
-		Integer expectedLength = 10, expectedData = 7;
+		Integer expectedLength = 10, expectedData = 7, expectedEncoded = 16;
 		Integer results[] = DayEight.process("\"aaa\\\"aaa\"");
 
 		assertEquals(expectedLength, results[0]);
 		assertEquals(expectedData, results[1]);
+		assertEquals(expectedEncoded, results[2]);
 	}
 
 	@Test
 	public void test4() {
-		Integer expectedLength = 6, expectedData = 1;
+		Integer expectedLength = 6, expectedData = 1, expectedEncoded = 11;
 		Integer results[] = DayEight.process("\"\\x27\"");
 
 		assertEquals(expectedLength, results[0]);
 		assertEquals(expectedData, results[1]);
+		assertEquals(expectedEncoded, results[2]);
 	}
 
 	@Test
-	public void testPart1Answer() {
-		Integer expectedLength = 6195, expectedData = 4845;
-		Integer results[] = { 0, 0 };
+	public void testAnswers() {
+		Integer expectedLength = 6195, expectedData = 4845, expectedEncoded = 8280;
+		Integer results[] = { 0, 0, 0 };
 		List<Integer[]> fileInput = new ArrayList<Integer[]>();
 
 		try (Scanner input = new Scanner(new File("Resources/Input/Eight.txt"), "UTF-8")) {
@@ -64,64 +68,11 @@ public class DayEightTest {
 		for (Integer[] integers : fileInput) {
 			results[0] = results[0] + integers[0];
 			results[1] = results[1] + integers[1];
+			results[2] = results[2] + integers[2];
 		}
 
 		assertEquals(expectedLength, results[0]);
 		assertEquals(expectedData, results[1]);
-	}
-
-	@Test
-	public void test5() {
-		Integer total = 6;
-		Integer results[] = DayEight.process("\"\"");
-
-		assertEquals(total, results[2]);
-	}
-
-	@Test
-	public void test6() {
-		Integer total = 9;
-		Integer results[] = DayEight.process("\"abc\"");
-
-		assertEquals(total, results[2]);
-	}
-
-	@Test
-	public void test7() {
-		Integer total = 16;
-		Integer results[] = DayEight.process("\"aaa\\\"aaa\"");
-
-		assertEquals(total, results[2]);
-	}
-
-	@Test
-	public void test8() {
-		Integer total = 11;
-		Integer results[] = DayEight.process("\"\\x27\"");
-
-		assertEquals(total, results[2]);
-	}
-
-	@Test
-	public void testPart2Answer() {
-		Integer expectedLength = 6195, expectedData = 8280;
-		Integer results[] = { 0, 0 };
-		List<Integer[]> fileInput = new ArrayList<Integer[]>();
-
-		try (Scanner input = new Scanner(new File("Resources/Input/Eight.txt"), "UTF-8")) {
-			while (input.hasNext()) {
-				fileInput.add(DayEight.process(input.nextLine()));
-			}
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-
-		for (Integer[] integers : fileInput) {
-			results[0] = results[0] + integers[0];
-			results[1] = results[1] + integers[2];
-		}
-
-		assertEquals(expectedLength, results[0]);
-		assertEquals(expectedData, results[1]);
+		assertEquals(expectedEncoded, results[2]);
 	}
 }
